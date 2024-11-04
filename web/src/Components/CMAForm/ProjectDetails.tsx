4bd401376f6eed04b04a620bc642124281311119
Ravindu Fernando <ravinduf@xeptagon.com>
2024-11-04 14:13:02 +0530

    SLCR-72: fixed issue in website field in inf

--- web/src/Components/CMAForm/ProjectDetails.tsx
+++ web/src/Components/CMAForm/ProjectDetails.tsx
@@ -31,7 +31,7 @@ const ProjectDetails = (props: CustomStepsProps) => {
         telephone: values?.telephone,
         physicalAddress: values?.physicalAddress,
         email: values?.email,
-        website: values?.website,
+        website: 'https://' + values?.website,
       },
     };
 
@@ -240,6 +240,7 @@ const ProjectDetails = (props: CustomStepsProps) => {
                     <Form.Item
                       label={t('CMAForm:website')}
                       name="website"
+                      className="website-input"
                       rules={[
                         {
                           required: true,
@@ -260,7 +261,7 @@ const ProjectDetails = (props: CustomStepsProps) => {
                         },
                       ]}
                     >
-                      <Input size="large" disabled={disableFields} />
+                      <Input size="large" disabled={disableFields} addonBefore="https://" />
                     </Form.Item>
                   </div>
                 </Col>
