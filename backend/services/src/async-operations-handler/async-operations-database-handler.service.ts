27eb4b69fac4d7a1a640178e93ce12bd0df5bc36
leshanw <leshanw@xeptagon.com>
2024-11-06 15:15:01 +0530

    SLCR-106: Fixing article 6 project creation flow

--- backend/services/src/async-operations-handler/async-operations-database-handler.service.ts
+++ backend/services/src/async-operations-handler/async-operations-database-handler.service.ts
@@ -1,27 +1,25 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { AsyncOperationsHandlerInterface } from './async-operations-handler-interface.service';
-import { AsyncOperationsHandlerService } from './async-operations-handler.service';
-import { AsyncActionEntity } from '../entities/async.action.entity';
-import { CounterType } from '../util/counter.type.enum';
-import { Counter } from '../entities/counter.entity';
+import { Injectable, Logger } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { AsyncOperationsHandlerInterface } from "./async-operations-handler-interface.service";
+import { AsyncOperationsHandlerService } from "./async-operations-handler.service";
+import { AsyncActionEntity } from "../entities/async.action.entity";
+import { CounterType } from "../util/counter.type.enum";
+import { Counter } from "../entities/counter.entity";
 
 @Injectable()
-export class AsyncOperationsDatabaseHandlerService
-  implements AsyncOperationsHandlerInterface
-{
+export class AsyncOperationsDatabaseHandlerService implements AsyncOperationsHandlerInterface {
   constructor(
     private logger: Logger,
     @InjectRepository(Counter) private counterRepo: Repository<Counter>,
     @InjectRepository(AsyncActionEntity)
     private asyncActionRepo: Repository<AsyncActionEntity>,
-    private asyncOperationsHandlerService: AsyncOperationsHandlerService,
+    private asyncOperationsHandlerService: AsyncOperationsHandlerService
   ) {}
 
   async asyncHandler(event: any): Promise<any> {
-    this.logger.log('database asyncHandler started', JSON.stringify(event));
-  
+    this.logger.log("database asyncHandler started", JSON.stringify(event));
+
     const seqObj = await this.counterRepo.findOneBy({
       id: CounterType.ASYNC_OPERATIONS,
     });
@@ -32,23 +30,23 @@ export class AsyncOperationsDatabaseHandlerService
     let retryCount = 0;
     const retryLimit = 50;
     const baseDelay = 5000; // Initial delay for exponential backoff
-  
+
     const doActions = async () => {
-      console.log('lastSeq', lastSeq, 'retryCount', retryCount);
+      console.log("lastSeq", lastSeq, "retryCount", retryCount);
       const notExecutedActions = await this.asyncActionRepo
-        .createQueryBuilder('asyncAction')
-        .where('asyncAction.actionId > :lastExecuted', { lastExecuted: lastSeq })
-        .orderBy('"actionId"', 'ASC')
+        .createQueryBuilder("asyncAction")
+        .where("asyncAction.actionId > :lastExecuted", { lastExecuted: lastSeq })
+        .orderBy('"actionId"', "ASC")
         .select(['"actionId"', '"actionType"', '"actionProps"'])
         .getRawMany();
-  
+
       if (notExecutedActions.length !== 0) {
         try {
-            for (const action of notExecutedActions) {
-            console.log('Action start', action.actionType, action.actionId);
+          for (const action of notExecutedActions) {
+            console.log("Action start", action.actionType, action.actionId);
             await this.asyncOperationsHandlerService.handler(
               action.actionType,
-              JSON.parse(action.actionProps),
+              JSON.parse(action.actionProps)
             );
             lastSeq = action.actionId;
             await this.counterRepo.save({
@@ -58,9 +56,9 @@ export class AsyncOperationsDatabaseHandlerService
             retryCount = 0; // Reset retry count after a successful execution
           }
         } catch (exception) {
-          this.logger.log('database asyncHandler failed', exception);
+          this.logger.log("database asyncHandler failed", exception);
           if (retryCount >= retryLimit) {
-            this.logger.log('database asyncHandler terminated');
+            this.logger.log("database asyncHandler terminated");
             return;
           }
           const delay = baseDelay * Math.pow(2, retryCount); // Exponential backoff
@@ -73,7 +71,7 @@ export class AsyncOperationsDatabaseHandlerService
       // Schedule the next execution after a fixed delay regardless of success or failure
       setTimeout(doActions, baseDelay);
     };
-  
+
     await doActions();
   }
 }
--- backend/services/src/async-operations/async-operations-database.service.ts
+++ backend/services/src/async-operations/async-operations-database.service.ts
@@ -5,15 +5,10 @@ import { Repository } from "typeorm";
 import { AsyncActionEntity } from "../entities/async.action.entity";
 import { AsyncActionType } from "../enum/async.action.type.enum";
 import { HelperService } from "../util/helpers.service";
-import {
-  AsyncAction,
-  AsyncOperationsInterface,
-} from "./async-operations.interface";
+import { AsyncAction, AsyncOperationsInterface } from "./async-operations.interface";
 
 @Injectable()
-export class AsyncOperationsDatabaseService
-  implements AsyncOperationsInterface
-{
+export class AsyncOperationsDatabaseService implements AsyncOperationsInterface {
   private emailDisabled: boolean;
 
   constructor(
@@ -26,37 +21,61 @@ export class AsyncOperationsDatabaseService
     this.emailDisabled = this.configService.get<boolean>("email.disabled");
   }
 
-  public tx:AsyncAction[]=[]
+  public tx: AsyncAction[] = [];
 
-  public async flushTx(): Promise<boolean>{
-    for (var action of this.tx){
+  public async flushTx(): Promise<boolean> {
+    for (var action of this.tx) {
       //execute action
     }
-    this.tx=[]
-    return true
+    this.tx = [];
+    return true;
   }
 
   public async AddAction(action: AsyncAction): Promise<boolean> {
-
-    if ([AsyncActionType.DocumentUpload, AsyncActionType.IssueCredit, AsyncActionType.RegistryCompanyCreate, AsyncActionType.RejectProgramme,AsyncActionType.AddMitigation,AsyncActionType.ProgrammeAccept,AsyncActionType.ProgrammeCreate,AsyncActionType.OwnershipUpdate, AsyncActionType.CompanyUpdate].includes(action.actionType) && !this.configService.get("registry.syncEnable")) {
-      this.logger.log(`Dropping sync event ${action.actionType} due to sync disabled`)
+    if (
+      [
+        AsyncActionType.DocumentUpload,
+        AsyncActionType.IssueCredit,
+        AsyncActionType.RegistryCompanyCreate,
+        AsyncActionType.RejectProgramme,
+        AsyncActionType.AddMitigation,
+        AsyncActionType.ProgrammeAccept,
+        AsyncActionType.ProgrammeCreate,
+        AsyncActionType.OwnershipUpdate,
+        AsyncActionType.CompanyUpdate,
+      ].includes(action.actionType) &&
+      !this.configService.get("registry.syncEnable")
+    ) {
+      this.logger.log(`Dropping sync event ${action.actionType} due to sync disabled`);
       return false;
     }
-    
+
     if (action.actionType === AsyncActionType.Email) {
       if (this.emailDisabled) return false;
     }
-    
+
+    if (
+      [
+        AsyncActionType.CADTProgrammeCreate,
+        AsyncActionType.CADTCertify,
+        AsyncActionType.CADTCreditIssue,
+        AsyncActionType.CADTTransferCredit,
+        AsyncActionType.CADTUpdateProgramme,
+      ].includes(action.actionType) &&
+      !this.configService.get("cadTrust.enable")
+    ) {
+      return false;
+    }
+
     let asyncActionEntity: AsyncActionEntity = {} as AsyncActionEntity;
     asyncActionEntity.actionType = action.actionType;
     asyncActionEntity.actionProps = JSON.stringify(action.actionProps);
     await this.asyncActionRepo.save(asyncActionEntity).catch((err: any) => {
       this.logger.error("error", err);
       throw new HttpException(
-        this.helperService.formatReqMessagesString(
-          "common.addAsyncActionDatabaseFailed",
-          ["Email"]
-        ),
+        this.helperService.formatReqMessagesString("common.addAsyncActionDatabaseFailed", [
+          "Email",
+        ]),
         HttpStatus.INTERNAL_SERVER_ERROR
       );
     });
