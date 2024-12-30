e67df8f0d540fd386396a72b48982fc853b34943
leshanw <leshanw@xeptagon.com>
2024-12-30 14:54:58 +0530

    SLCR-225: Fixed issue when ordering dashboard credits by purpose BE

--- backend/services/src/analytics-api/aggregate.sl.api.service.ts
+++ backend/services/src/analytics-api/aggregate.sl.api.service.ts
@@ -1696,7 +1696,7 @@ export class AggregateSlAPIService {
       .addSelect("SUM(programme.creditEst)", "total_credit_authorised")
       .where(this.helperService.generateWhereSQL(query, null))
       .groupBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')")
-      .orderBy("log_date", "ASC")
+      .orderBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')", "ASC")
       .getRawMany();
 
     const issuedLogType = {
@@ -1715,7 +1715,7 @@ export class AggregateSlAPIService {
       .addSelect("SUM((audit_log.data->>'creditIssued')::numeric)", "total_credit_issued")
       .where(this.helperService.generateWhereSQL(query, null))
       .groupBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')")
-      .orderBy("log_date", "ASC")
+      .orderBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')", "ASC")
       .getRawMany();
 
     const transferredLogType = {
@@ -1734,7 +1734,7 @@ export class AggregateSlAPIService {
       .addSelect("SUM((audit_log.data->>'creditAmount')::numeric)", "total_credit_transferred")
       .where(this.helperService.generateWhereSQL(query, null))
       .groupBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')")
-      .orderBy("log_date", "ASC")
+      .orderBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')", "ASC")
       .getRawMany();
 
     const retiredLogType = {
@@ -1752,7 +1752,7 @@ export class AggregateSlAPIService {
       .addSelect("SUM((audit_log.data->>'creditAmount')::numeric)", "total_credit_retired")
       .where(this.helperService.generateWhereSQL(query, null))
       .groupBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')")
-      .orderBy("log_date", "ASC")
+      .orderBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')", "ASC")
       .getRawMany();
 
     const data = {
@@ -1822,7 +1822,7 @@ export class AggregateSlAPIService {
       .where(this.helperService.generateWhereSQL(query, null))
       .groupBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')")
       .addGroupBy("programme.purposeOfCreditDevelopment")
-      .orderBy("logDate", "ASC")
+      .orderBy("TO_CHAR(TO_TIMESTAMP(audit_log.createdTime / 1000), 'YYYY-MM-DD')", "ASC")
       .getRawMany();
 
     return new DataResponseDto(HttpStatus.OK, issuedCreditsByDate);
