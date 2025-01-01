92f71066a44398d84d80f65feae203b2c27aed5d
Amila Dissanayake <amilad@xeptagon.com>
2025-01-01 15:44:22 +0530

    HG-8 Create project structure

--- apps/api-service/src/main.ts
+++ apps/api-service/src/main.ts
@@ -3,6 +3,6 @@ import { ApiServiceModule } from "./api-service.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiServiceModule);
-  await app.listen(3001);
+  await app.listen(process.env.API_SERVICE_PORT || 3001);
 }
 bootstrap();
--- apps/custodian-service/src/main.ts
+++ apps/custodian-service/src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { CustodianServiceModule } from './custodian-service.module';
+import { NestFactory } from "@nestjs/core";
+import { CustodianServiceModule } from "./custodian-service.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(CustodianServiceModule);
-  await app.listen(3000);
+  await app.listen(process.env.CUSTODIAN_SERVICE_PORT || 3002);
 }
 bootstrap();
