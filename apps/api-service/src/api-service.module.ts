9a3e1cd9dd775a0dd9b5afabcc2f189ef34ccbd9
Amila Dissanayake <amilad@xeptagon.com>
2025-01-02 16:17:39 +0530

    HG-8 Adding entities required for api service

--- apps/api-service/src/api-service.module.ts
+++ apps/api-service/src/api-service.module.ts
@@ -5,9 +5,21 @@ import { CoreModule } from "libs/core/core.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SharedModule } from "libs/shared/shared.module";
 import { UserEntity } from "libs/shared/entities/user.entity";
+import { OrganizationEntity } from "libs/shared/entities/organization.entity";
+import { UserRoleEntity } from "libs/shared/entities/user.role.entity";
+import { OrganizationTypeEntity } from "libs/shared/entities/organization.type.entity";
 
 @Module({
-  imports: [CoreModule, SharedModule, TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    CoreModule,
+    SharedModule,
+    TypeOrmModule.forFeature([
+      UserEntity,
+      OrganizationEntity,
+      UserRoleEntity,
+      OrganizationTypeEntity,
+    ]),
+  ],
   controllers: [ApiServiceController],
   providers: [ApiServiceService],
 })
