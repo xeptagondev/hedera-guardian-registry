8b0aab3fd90bcbed50d7df3bcddbdd3963728313
OshanM <oshanm@xeptagon.com>
2025-01-17 12:13:37 +0530

    - Fixed module import

--- backend-hedera/libs/custodian-lib/src/shared/audit/audit.module.ts
+++ backend-hedera/libs/custodian-lib/src/shared/audit/audit.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuditService } from './service/audit.service';
+import { AuditEntity } from './entity/audit.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     providers: [AuditService],
     exports: [AuditService],
+    imports: [TypeOrmModule.forFeature([AuditEntity])],
 })
 export class AuditModule {}
